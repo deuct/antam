678a732ef5b67e0d8d8af78d021c4fc7a2e7b2b2
farhannfdl <farhan972015@gmail.com>
2022-08-15 18:58:56 +0700

    commit one

---
+++ index.php
@@ -0,0 +1,334 @@
+<?php include('header.php') ?>
+<body>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="mx-auto nav-flex">
+        <div class="collapse navbar-collapse" id="navbarText">
+          <a class="navbar-brand" href="#">
+            <img src="./assets/logo1.png" height="100px" />
+          </a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                SURAT
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Surat Masuk</a></li>
+                <li><a class="dropdown-item" href="#">Surat Keluar</a></li>
+                <li><a class="dropdown-item" href="#">Penasihat Dekom</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                LAPORAN
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">ANR</a></li>
+                <li><a class="dropdown-item" href="#">Laporan Kinerja</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                MATERI RAPAT
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">K. Audit</a></li>
+                <li><a class="dropdown-item" href="#">K. Pemantau Risk</a></li>
+                <li><a class="dropdown-item" href="#">K. GLGNR</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                PROYEK STRATEGI
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">P3FIT / Haltim</a></li>
+                <li><a class="dropdown-item" href="#">Mempawah (SGAR)</a></li>
+                <li><a class="dropdown-item" href="#">CGA Tayan</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ATURAN PERUSAHAAN</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2 ms-3 form-search"
+              type="search"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div id="header">
+        <img
+          src="./assets/Background Miring Tower.png"
+          style="position: absolute; top: -10px; z-index: 2; left: 0"
+          width="1400px"
+          height="1000px"
+        />
+        <img
+          src="./assets/Treuk Kuning.png"
+          style="position: absolute; top: 150px; right: -100px"
+          width="800px"
+        />
+        <img
+          src="./assets/Shape Bulet.png"
+          width="400px"
+          style="
+            position: absolute;
+            right: -50px;
+            top: -160px;
+            z-index: 50;
+            opacity: 0.7;
+          "
+        />
+        <div class="col-lg-4 col-md-4 col-sm-12">
+          <div class="header-fill">
+            <h1 class="mb-3">
+              <span class="hf-h1">SYNERGIC</span> <br /><span class="hf-h1"
+                >FOR</span
+              ><br /><span class="hf-h1">INDONESIA</span>
+            </h1>
+            <h3 class="hf-h3 mb-3">ANTAM'S VISION 2030</h3>
+            <p class="col-lg-5 col-md-5 col-sm-12 mb-3">
+              To become a leading global corporation through diversification and
+              integrated natural-resource based business
+            </p>
+            <div class="text-left ms-5">
+              <button class="btn btn-header">More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="row justify-content-center">
+      <div class="col-lg-8 col-md-8 col-sm-12 text-center">
+        <div class="description">
+          <img src="./assets/logo2.png" height="120px" />
+          <p>
+            ANTAM is a mining company which processes a number of minerals as
+            well as operating other business including trade and industry,
+            transportation and other services related with the mining sector.
+            ANTAM operating segments are distinguished according to three core
+            business which comprised on nickel, gold, and refinery, and bauxite
+            and alumina. ANTAM also has other segment and Head Office segment.
+            ANTAM also has others and Head Office segments.
+          </p>
+        </div>
+      </div>
+      <div class="divider-desc"></div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+        <div class="services-background">
+          <img
+            src="./assets/Shape Bulet.png"
+            height="800px"
+            style="
+              position: absolute;
+              z-index: 10;
+              left: -200px;
+              top: -400px;
+              opacity: 0.8;
+            "
+          />
+        </div>
+        <div id="services" class="mt-5">
+          <h3 class="my-3">ANTAM PRODUCT & SERVICE</h3>
+          <!-- <div class="btn-containerz">
+            <button class="pre-btn d-inline-block">
+              <img src="./right-arrow.png" alt="" />
+            </button>
+            <button class="nxt-btn d-inline-block">
+              <img src="./right-arrow.png" alt="" />
+            </button>
+          </div> -->
+          <div class="card-containerz">
+            <div class="d-inline-block card-gap">
+              <div class="card">
+                <img src="./assets/Exploration Service.png" width="280px" />
+                <!-- <div class="card-black"></div> -->
+                <div class="card-fill p-4">
+                  <div class="cf-2">
+                    <h3 class="cf-h3">EXPLO-RATION SERVICE</h3>
+                    <button class="btn btn-card">More</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="d-inline-block card-gap">
+              <div class="card">
+                <img src="./assets/Gold & Precious.png" />
+                <div class="card-black"></div>
+                <div class="card-fill p-4">
+                  <div class="cf-2">
+                    <h3 class="cf-h3">GOLD & PRECIOUS METAL REFINING</h3>
+                    <button class="btn btn-card">More</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="d-inline-block card-gap">
+              <div class="card">
+                <img src="./assets/Coal Mining.png" />
+                <div class="card-black"></div>
+                <div class="card-fill p-4">
+                  <div class="cf-2">
+                    <h3 class="cf-h3">COAL MINING</h3>
+                    <button class="btn btn-card">More</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <div id="gallery">
+        <img
+          src="./assets/Background Galeri.png"
+          style="object-fit: cover; width: 100%; height: 100%"
+        />
+        <h1>GALLERY</h1>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div id="news">
+        <div class="col-lg-3 col-md-3 col-sm-12 d-inline-block news-detail">
+          <h3>NEWS</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            varius ipsum sed nulla ultricies, sodales rutrum tellus mattis. Nam
+            imperdiet enim tincidunt risus volutpat, sit amet dictum lorem
+            interdum.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 d-inline-block news-detail">
+          <h3>EVENT</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            varius ipsum sed nulla ultricies, sodales rutrum tellus mattis. Nam
+            imperdiet enim tincidunt risus volutpat, sit amet dictum lorem
+            interdum.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 d-inline-block news-detail">
+          <h3>CSR</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            varius ipsum sed nulla ultricies, sodales rutrum tellus mattis. Nam
+            imperdiet enim tincidunt risus volutpat, sit amet dictum lorem
+            interdum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="strategy">
+      <img src="./assets/Shape Bulet.png" style="position: absolute;right:-160px;top:-300px; width: 400px; opacity: .8;">
+    <div class="row">
+      <div class="strategy-bg"></div>
+      <div class="col-lg-5 col-md-5 col-sm-12 my-5 ms-5 mx-3 strategy-fill">
+        <h2>Our Strategy</h2>
+        <p>
+          Our main objective is to enhance shareholder value by lowering costs
+          while profitably expanding operations in a sustainable manner.
+          <br />
+          <br />
+          Our strategy is to focus on our core business. We ask ourselves, "how
+          can we generate the most value from our reserves?" We build on our
+          existing strengths to ensure long term profitability. By maximizing
+          output we increase cash generation and lower unit costs.
+          <br />
+          <br />
+          We sustain growth through reliable expansion projects, strategic
+          alliances, acquisitions, increasing quiality reserves and adding value
+          by moving away from selling raw materials and increasing processing
+          activities.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div id="footer">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 text-start">
+          <img src="./assets/logo1.png" width="190px" style="margin-left: 0" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            consequuntur tempora porro cupiditate modi est.
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+          <div class="footer-icon">
+            <a href="#">
+              <img src="./assets/Facebook.png" />
+            </a>
+            <a href="#">
+              <img src="./assets/Twitter.png" />
+            </a>
+            <a href="#">
+              <img src="./assets/Instagram.png" />
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 footer-link text-end mt-5">
+          <ul>
+            <li><a href=#">SITEMAP</a></li>
+            <li><a href=#">ABOUT</a></li>
+            <li><a href=#">INFORMATION</a></li>
+            <li><a href=#">OTHER</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include('footer.php') ?>
+</body>
